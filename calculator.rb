4dcd4a3343055960e4a27d13c1e51d7c6a20f336
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2019-08-17 02:39:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,5 @@ first_number = 7
 second_number = 9
 sum = first_number + second_number
 difference = first_number - second_number
-pro = first_number * second_number
+product = first_number * second_number
 div = first_number / second_number
